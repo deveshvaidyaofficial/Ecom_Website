35040cfcea8dac3327b85a9b3adb1624a2c35ffe
deveshvaidyaofficial <deveshvaidya@outlook.com>
2026-07-08 16:52:31 +0530

    made changes in product display

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,22 +1,52 @@
-import styles from "./ProductCard.module.css";
+import "./ProductCard.css";
+
 function ProductCard({ product }) {
+
   return (
+
     <div className="product-card">
-      <div classname={styles.wishlist}>♡</div>
 
-      <div className="image-container">
-        <img src={product.image} alt={product.title} />
+      <button className="heart">
+
+        ♡
+
+      </button>
+
+      <div className="product-image">
+
+        <img
+          src={product.image}
+          alt={product.title}
+        />
+
       </div>
 
-      <div className="product-info">
-        <p className="price">{product.price}</p>
-        <h4>{product.title}</h4>
-        <p className="category">{product.category}</p>
-        <p className="colors">{product.colors}</p>
-        <p className="offer">Buy 2 get 50% off</p>
+      <div className="product-details">
+
+        <div className="price">
+
+          {product.price}
+
+        </div>
+
+        <h3>{product.title}</h3>
+
+        <p>{product.category}</p>
+
+        <span>{product.colors}</span>
+
+        <small>
+
+          Buy 2 get 50% off
+
+        </small>
+
       </div>
+
     </div>
+
   );
+
 }
 
 export default ProductCard;
--- src/data/products.js
+++ src/data/products.js
@@ -1,33 +1,84 @@
-const products = [
-  {
-    id: 1,
-    title: "Galaxy 7 Running Shoes",
-    price: "₹4,409.50",
-    category: "Men Performance",
-    colors: "7 colours",
-    image: "/products/shoe1.jfif"  },
-  {
-    id: 2,
-    title: "SAMBA OG Shoes",
-    price: "₹7,699.50",
-    category: "Women Originals",
-    colors: "13 colours",
-    image: "/products/shoe2.jfif"  },
-  {
-    id: 3,
-    title: "INDIA T20 Jersey",
-    price: "₹699.50",
-    category: "Men Performance",
-    colors: "",
-    image: "/products/jersey.jfif",
-  },
-  {
-    id: 4,
-    title: "Runfalcon Shoes",
-    price: "₹4,619.50",
-    category: "Men Performance",
-    colors: "7 colours",
-    image: "/products/shoe3.jfif",
-  },
+import shoe1 from "../assets/products/shoe1.png";
+import shoe2 from "../assets/products/shoe2.png";
+import shoe3 from "../assets/products/shoe3.png";
+import shoe4 from "../assets/products/shoe4.png";
+import shoe5 from "../assets/products/shoe5.png";
+import shoe6 from "../assets/products/shoe6.png";
+import shoe7 from "../assets/products/shoe7.png";
+import jersey from "../assets/products/jersey.png";
+
+export default [
+
+{
+id:1,
+title:"Galaxy 7 Running Shoes",
+price:"₹4,409.50",
+category:"Men Performance",
+colors:"7 colours",
+image:shoe1
+},
+
+{
+id:2,
+title:"SAMBA OG Shoes",
+price:"₹7,699.50",
+category:"Women Originals",
+colors:"13 colours",
+image:shoe2
+},
+
+{
+id:3,
+title:"INDIA T20 INTERNATIONAL CRICKET FAN JERSEY",
+price:"₹699.50",
+category:"Men Performance",
+colors:"",
+image:jersey
+},
+
+{
+id:4,
+title:"Runfalcon Shoes",
+price:"₹4,619.50",
+category:"Men Performance",
+colors:"7 colours",
+image:shoe3
+},
+
+{
+id:5,
+title:"Samba OG Shoes",
+price:"₹7,099.50",
+category:"Women Originals",
+colors:"13 colours",
+image:shoe4
+},
+
+{
+id:6,
+title:"Baseline Shoes",
+price:"₹2,579.50",
+category:"Sportswear",
+colors:"4 colours",
+image:shoe5
+},
+
+{
+id:7,
+title:"POMAZOR Shoes",
+price:"₹2,159.50",
+category:"Men Sportswear",
+colors:"3 colours",
+image:shoe6
+},
+
+{
+id:8,
+title:"Runfalcon 5 Running Shoes",
+price:"₹4,619.50",
+category:"Men Performance",
+colors:"7 colours",
+image:shoe7
+}
+
 ];
-export default products;
