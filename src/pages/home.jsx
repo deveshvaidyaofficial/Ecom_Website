6b2ecc4527ffa97c0ca8c7b92c7d9ba80679b6c5
deveshvaidyaofficial <deveshvaidya@outlook.com>
2026-07-08 17:22:30 +0530

    updated the pages with their code

---
+++ src/pages/home.jsx
@@ -0,0 +1,23 @@
+import PromoBar from "../components/PromoBar/PromoBar";
+import Navbar from "../components/Navbar/Navbar";
+import HeroSection from "../components/HeroSection/HeroSection";
+import CategorySection from "../components/CategorySection/CategorySection";
+import ProductSection from "../components/ProductSection/ProductSection";
+import Newsletter from "../components/Newsletter/Newsletter";
+import Footer from "../components/Footer/Footer";
+
+function Home() {
+  return (
+    <>
+      <PromoBar />
+      <Navbar />
+      <HeroSection />
+      <CategorySection />
+      <ProductSection />
+      <Newsletter />
+      <Footer />
+    </>
+  );
+}
+
+export default Home;
---
+++ src/pages/product.jsx
@@ -0,0 +1,50 @@
+import { useParams } from "react-router-dom";
+
+import Navbar from "../components/Navbar/Navbar";
+import Footer from "../components/Footer/Footer";
+
+import products from "../data/products";
+
+function Product() {
+
+  const { product_id } = useParams();
+
+  const product = products.find(
+    p => p.id === Number(product_id)
+  );
+
+  if (!product) {
+    return <h2>Product Not Found</h2>;
+  }
+
+  return (
+    <>
+      <Navbar />
+
+      <div className="product-page">
+
+        <img
+          src={product.image}
+          alt={product.title}
+        />
+
+        <div>
+
+          <h1>{product.title}</h1>
+
+          <h2>{product.price}</h2>
+
+          <p>{product.category}</p>
+
+          <p>{product.colors}</p>
+
+        </div>
+
+      </div>
+
+      <Footer />
+    </>
+  );
+}
+
+export default Product;
