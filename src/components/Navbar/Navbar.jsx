56cb9adb0b581b141a1008a061285f4e45ffdc00
deveshvaidyaofficial <deveshvaidya@outlook.com>
2026-07-09 17:24:33 +0530

    reverted navbar chages..new version unstable

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,11 +3,18 @@ import "./Navbar.css";
 function Navbar() {
   return (
     <nav className="navbar">
+      <div className="nav-top">
+        <span>help</span>
+        <span>orders and returns</span>
+        <span>sign up</span>
+        <span>log in</span>
+      </div>
+
       <div className="nav-main">
         <div className="logo">
           <img src="https://upload.wikimedia.org/wikipedia/commons/2/20/Adidas_Logo.svg" width="100" height="50" alt="Adidas Logo" />
         </div>
-        <br></br>
+
         <ul className="nav-links">
           <li>MEN</li>
           <li>WOMEN</li>
@@ -16,6 +23,7 @@ function Navbar() {
           <li>LIFESTYLE</li>
           <li className="outlet">OUTLET</li>
         </ul>
+
         <div className="nav-icons">
           <span>🔍</span>
           <span>👤</span>
